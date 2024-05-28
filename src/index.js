075a45eb8ceb78d5988537551d9c4f779ccf57e9
Hung Cung <hung.cung@niteco.se>
2024-05-28 14:39:58 +0700

    using gemini

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
+import 'dotenv/config'
 import express from 'express';
 import { sequelize } from './db'
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+const schema = (keyword) => ({
+  title: `Only 5 words which are synonym of '${keyword}'`,
+  description:
+    `Return only 5 words which are synonym of '${keyword}'`,
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      word: { description: "word" },
+      context: {description: "context of using this word"},
+      example: { description: "example of using this word"},
+      ipa: {description: "IPA"},
+    },
+  }
+});
+
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: {
+    responseMimeType: "application/json"
+  }});
+
+  const prompt = `Follow JSON schema.<JSONSchema>${JSON.stringify(
+    schema("play")
+  )}</JSONSchema>`
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log("result", text);
+}
+
+run();
 
 sequelize.sync().then(() => {
     console.log("Connect DB success")
 })
 const app = express()
 const port = 3001
+
 
 app.get('/', (req, res) => {
   res.send('Hello World1!')
